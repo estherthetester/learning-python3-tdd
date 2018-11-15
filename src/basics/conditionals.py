94bc7e71b58c9ceab40d91c57687d32a9cfdac80
Esther Lloyd <esthermaylloyd@gmail.com>
2018-11-15 21:43:19 +0000

    feat(conditionals): Learning conditionals and mods
    This time it was easy! Same logic.

--- src/basics/conditionals.py
+++ src/basics/conditionals.py
@@ -1,18 +1,16 @@
-#Task: if choice is 7 print out lucky, otherwise print unlucky
-
-import unittest.mock as mock
+from unittest.mock import patch
 import random
 
-
+#Task 1: if choice is 7 print out lucky, otherwise print unlucky
 def test_check_lucky(capsys):
-    with mock.patch('random.randint', return_value=7):
+    with patch('random.randint', return_value=7):
         check_lucky()
         captured = capsys.readouterr()
         assert "lucky" == captured.out.rstrip()
 
 
 def test_check_unlucky(capsys):
-    with mock.patch('random.randint', return_value=10):
+    with patch('random.randint', return_value=10):
         check_lucky()
         captured = capsys.readouterr()
         assert "unlucky" == captured.out.rstrip()
@@ -24,6 +22,30 @@ def check_lucky():
         print("lucky")
     else:
         print("unlucky")
+
+
+#Task 2: if number is odd print odd otherwise print even
+def check_odd():
+    num = random.randint(1, 1000)
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+def test_check_odd(capsys):
+    with patch('random.randint', return_value=1):
+        check_odd()
+        captured = capsys.readouterr()
+        assert "odd" == captured.out.rstrip()
+
+
+def test_check_even(capsys):
+    with patch('random.randint', return_value=900):
+        check_odd()
+        captured = capsys.readouterr()
+        assert "even" == captured.out.rstrip()
+
 
 
 check_lucky()
