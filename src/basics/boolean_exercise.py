d303114b73a3902a95c8697b43202c19a474739d
Esther Lloyd <esthermaylloyd@gmail.com>
2018-11-19 21:43:03 +0000

    feat(booleans): We now take into account sick days remaining, and if we love our job
    Changed how to call the method, without having to mock out the randomness everytime. Much easier.

--- src/basics/boolean_exercise.py
+++ src/basics/boolean_exercise.py
@@ -1,5 +1,4 @@
-from random import choice, randint, seed
-from unittest.mock import patch
+from random import choice, randint
 
 
 def exercise_inputs():
@@ -11,18 +10,35 @@ def exercise_inputs():
 
 
 def calling_in_sick(actually_sick, kinda_sick, hate_your_job, sick_days):
-    if actually_sick and sick_days > 0:
+    if (actually_sick and sick_days > 0) or (kinda_sick and hate_your_job and sick_days > 0):
         return True
+    else:
+        return False
 
 
-def test_calling_in_sick():
-    with patch('boolean_exercise.choice', side_effect=[True, True, True]):
-        seed(1)
-        test_actually_sick, test_kinda_sick, test_hate_your_job, test_sick_days = exercise_inputs()
-        assert calling_in_sick(test_actually_sick, test_kinda_sick, test_hate_your_job, test_sick_days)
+def test_calling_in_sick_actually_sick():
+    assert calling_in_sick(True, False, True, 10)
+    assert calling_in_sick(True, False, False, 10)
+    assert calling_in_sick(True, True, False, 10)
 
+
+def test_calling_in_sick_kinda_sick():
+    assert calling_in_sick(False, True, True, 10)
+    assert calling_in_sick(True, True, False, 10)
+
+
+def test_going_to_work_no_leave():
+    assert not calling_in_sick(True, False, True, 0)
+    assert not calling_in_sick(True, False, False, 0)
+    assert not calling_in_sick(True, True, False, 0)
+    assert not calling_in_sick(True, True, True, 0)
+    assert not calling_in_sick(False, True, False, 0)
+
+
+def test_going_to_work_love_job():
+    assert not calling_in_sick(False, True, False, 10)
 
 
 # randomly assigns values to these four variables
-actually_sick, kinda_sick, hate_your_job, sick_days = exercise_inputs()
-calling_in_sick(actually_sick, kinda_sick, hate_your_job, sick_days)
+really_sick, fake_sick, hate_job, days = exercise_inputs()
+calling_in_sick(really_sick, fake_sick, hate_job, days)
