d974ef6523e5eb34ac4009a40cb96a2319f41202
Esther Lloyd <esthermaylloyd@gmail.com>
2018-11-21 22:25:24 +0000

    feat(rockpaperscissors): Now it is actually a playable game
    Note that this is following the course work, and the horrible if statements of death will be destroyed. With fire.

--- src/basics/rock_paper_scissor/two_player.py
+++ src/basics/rock_paper_scissor/two_player.py
@@ -3,17 +3,6 @@ from unittest.mock import patch
 PLAYERS = ['Player 1', 'Player 2']
 
 
-def print_intro():
-    print('...rock...')
-    print('...paper...')
-    print('...scissors...')
-
-
-def get_player_input(player_name):
-    player_input = input("(enter " + str(player_name) + "'s choice): ")
-    return player_input
-
-
 def test_print_intro(capsys):
     print_intro()
     capture = capsys.readouterr()
@@ -29,15 +18,76 @@ def test_player_1_input():
 def test_get_input():
     test_inputs = ['rock', 'paper']
     with patch('builtins.input', side_effect=test_inputs):
-        assert test_inputs == play_game()
+        assert test_inputs[0] == get_inputs()[PLAYERS[0]]
 
 
-def play_game():
-    inputs = []
+def test_same_shoot():
+    test_inputs = ['rock', 'rock']
+    with patch('builtins.input', side_effect=test_inputs):
+        assert "Tie" == get_winner()
+
+
+def test_player_1_wins():
+    test_inputs = ['rock', 'scissors']
+    with patch('builtins.input', side_effect=test_inputs):
+        assert PLAYERS[0] == get_winner()
+
+
+def test_player_2_wins():
+    test_inputs = ['scissors', 'rock']
+    with patch('builtins.input', side_effect=test_inputs):
+        assert PLAYERS[1] == get_winner()
+
+
+def print_intro():
+    print('...rock...')
+    print('...paper...')
+    print('...scissors...')
+
+
+def get_player_input(player_name):
+    player_input = input("(enter " + str(player_name) + "'s choice): ")
+    return player_input
+
+
+def get_inputs():
+    player_inputs = {}
     for name in PLAYERS:
-        inputs.append(get_player_input(name))
+        player_inputs[name] = get_player_input(name)
     print("SHOOT!")
-    return inputs
+    return player_inputs
 
 
-play_game()
+def get_winner():
+    inputs = list(get_inputs().values())
+    winner = None
+    if inputs[0] == "rock" and inputs[1] == "scissors":
+        winner = PLAYERS[0]
+    elif inputs[0] == "rock" and inputs[1] == "paper":
+        winner = PLAYERS[1]
+    elif inputs[0] == "paper" and inputs[1] == "rock":
+        winner = PLAYERS[0]
+    elif inputs[0] == "paper" and inputs[1] == "scissors":
+        winner = PLAYERS[1]
+    elif inputs[0] == "scissors" and inputs[1] == "rock":
+        winner = PLAYERS[1]
+    elif inputs[0] == "scissors" and inputs[1] == "paper":
+        winner = PLAYERS[0]
+    elif inputs[0] == inputs[1]:
+        winner = 'Tie'
+    else:
+        print("Something went wrong.")
+    print_winner(winner)
+    return winner
+
+
+def print_winner(winner):
+    if winner != 'Tie':
+        print(f"{winner} wins!")
+    elif winner == 'Tie':
+        print("It's a tie!")
+    else:
+        print("There should always be a winner, something must have gone wrong.")
+
+
+get_winner()
