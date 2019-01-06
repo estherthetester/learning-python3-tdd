ae0a0e893dc81db5690b8c1cd3a42d6c2451f067
estherthetester <esthermaylloyd@gmail.com>
2019-01-06 19:05:18 +0000

    feat(repeater-exercise): Now takes an input and yells the given amount for the person to clean their room

--- src/basics/repeater_exercise/clean_your_room.py
+++ src/basics/repeater_exercise/clean_your_room.py
@@ -1,8 +1,19 @@
 #Take in an input and print the number of times to clean your room.
 #Defaults to printing once if no input given
 
-def print_clean_room(number_of_times=1):
 
+def print_clean_room(number_of_times=1):
     for num in range(number_of_times):
         print("CLEAN UP YOUR ROOM!")
 
+
+def number_of_times_to_tell():
+    return input("How many times do I have to tell you?")
+
+
+def main():
+    print_clean_room(number_of_times_to_tell())
+
+
+if __name__ == '__main__':
+    main()
--- src/basics/repeater_exercise/test_clean_your_room.py
+++ src/basics/repeater_exercise/test_clean_your_room.py
@@ -1,4 +1,5 @@
 import clean_your_room as clean
+from unittest.mock import patch
 
 
 def test_should_print_out_message(capsys):
@@ -18,3 +19,17 @@ def test_should_print_out_message_for_given_times(capsys):
 
     assert expected_output == capture.out.rstrip()
 
+
+def test_should_get_number_of_times_to_tell():
+    with patch('builtins.input', return_value = 2):
+        test = clean.number_of_times_to_tell()
+        assert 2 == test
+
+
+def test_should_tell_them_to_clean_room_two_times(capsys):
+    expected_output = "CLEAN UP YOUR ROOM!\nCLEAN UP YOUR ROOM!"
+
+    with patch('builtins.input', return_value = 2):
+        clean.main()
+        capture = capsys.readouterr()
+        assert expected_output == capture.out.rstrip()
